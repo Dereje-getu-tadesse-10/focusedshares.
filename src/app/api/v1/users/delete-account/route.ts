5945f148c08a2be1af2de2af68e379eb1f50c8f8
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-12-05 18:10:51 +0100

    feat: add delete account api route

---
+++ src/app/api/v1/users/delete-account/route.ts
@@ -0,0 +1,39 @@
+import { auth } from '@/src/lib/auth';
+import { prisma } from '@/src/lib/prisma';
+
+export const DELETE = async (request: Request) => {
+  const session = await auth();
+
+  if (!session) {
+    return Response.json(
+      {
+        message: 'You must be logged in',
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  const userExists = await prisma.user.findUnique({
+    where: {
+      id: session.user.id,
+    },
+  });
+
+  if (!userExists) {
+    return Response.json({
+      status: 404,
+    });
+  }
+
+  await prisma.user.delete({
+    where: {
+      id: session.user.id,
+    },
+  });
+
+  return Response.json({
+    status: 200,
+  });
+};
