e1aa679144c8a2cf9b41d34a0fb23bc8225118b4
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-12-05 18:56:41 +0100

    update: create custom hook for data mutation

---
+++ src/components/Forms/Youtube/genres.tsx
@@ -0,0 +1,44 @@
+export const GENRES = [
+  {
+    id: 'KPOP',
+    image:
+      'https://res.cloudinary.com/ddugf28mx/image/upload/f_auto,q_auto:low/v1/focusedshares/mty2rbolcjzfmowqngmx',
+    name: 'Kpop',
+  },
+  {
+    id: 'LOFI',
+    image:
+      'https://res.cloudinary.com/ddugf28mx/image/upload/f_auto,q_auto:low/v1/focusedshares/ymmmy7cikgaklud9brlf',
+    name: 'Lofi',
+  },
+  {
+    id: 'CLASSICAL',
+    image:
+      'https://res.cloudinary.com/ddugf28mx/image/upload/f_auto,q_auto:low/v1/focusedshares/oehr8iow9q0e8zbwe3oq',
+    name: 'Classical',
+  },
+  {
+    id: 'AMBIENT',
+    image:
+      'https://res.cloudinary.com/ddugf28mx/image/upload/f_auto,q_auto:low/v1/focusedshares/zj7h6bpqekjweyebdvff',
+    name: 'Ambient',
+  },
+  {
+    id: 'JAZZ',
+    image:
+      'https://res.cloudinary.com/ddugf28mx/image/upload/f_auto,q_auto:low/v1/focusedshares/y800oq30izhvps708qeu',
+    name: 'Jazz',
+  },
+  {
+    id: 'ELECTRONIC',
+    image:
+      'https://res.cloudinary.com/ddugf28mx/image/upload/f_auto,q_auto:low/v1/focusedshares/s5nc0o3zyuejbtatxzpc',
+    name: 'Electronic',
+  },
+  {
+    id: 'OTHER',
+    image:
+      'https://res.cloudinary.com/ddugf28mx/image/upload/f_auto,q_auto:low/v1/focusedshares/iiqv4aqmcplaenar0bgr',
+    name: 'Other',
+  },
+];
