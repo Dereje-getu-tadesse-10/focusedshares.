549cea2b32ed5c4905921233e781222f30c2a53e
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-12-05 15:36:56 +0100

    update: SessionProvider to Providers component

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,11 @@
 'use client';
+import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 
 export const Providers = ({ children }: React.PropsWithChildren<{}>) => {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <SessionProvider>
+      <ThemeProvider>{children}</ThemeProvider>
+    </SessionProvider>
+  );
 };
