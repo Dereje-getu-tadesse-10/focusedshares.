46cdcb3dc8ad00fe64085d81d3d7834a82eea0d9
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-12-06 08:10:08 +0100

    update: add @tanstack experimental & add Toaster provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,6 +4,8 @@ import NiceModal from '@ebay/nice-modal-react';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
+import { Toaster } from 'react-hot-toast';
 
 const queryClient = new QueryClient();
 
@@ -12,9 +14,12 @@ export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <SessionProvider>
       <QueryClientProvider client={client}>
-        <ThemeProvider defaultTheme='dark'>
-          <NiceModal.Provider>{children}</NiceModal.Provider>
-        </ThemeProvider>
+        <ReactQueryStreamedHydration>
+          <ThemeProvider defaultTheme='dark'>
+            <NiceModal.Provider>{children}</NiceModal.Provider>
+          </ThemeProvider>
+          <Toaster />
+        </ReactQueryStreamedHydration>
       </QueryClientProvider>
     </SessionProvider>
   );
