09017f8a4f111deb699aeb809f2fbb8aaba3e1f7
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-12-06 15:01:31 +0100

    update: runtime variable in route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,2 @@
 export { GET, POST } from '@/src/lib/auth';
-// export const runtime = 'edge'; only when app is deployed to cloudflare workers
+export const runtime = 'edge'; // only when app is deployed to cloudflare workers
