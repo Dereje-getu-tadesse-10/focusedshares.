a389ab154de85b9636f436931273d27acf9eea62
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-12-11 15:41:05 +0100

    update: run prettier

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,8 +3,9 @@ import { withAccelerate } from '@prisma/extension-accelerate';
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient({
-  // log: ['query', 'info', 'warn', 'error'],
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    // log: ['query', 'info', 'warn', 'error'],
   }).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
