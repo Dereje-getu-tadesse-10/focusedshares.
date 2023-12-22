9f02068385141d88cd001280eea26d9485d9113e
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-12-22 21:45:04 +0100

    refactoring: remove tanstack client provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,16 +1,11 @@
 'use client';
-import React, { useState } from 'react';
+import React from 'react';
 import NiceModal from '@ebay/nice-modal-react';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
 import { Toaster } from 'react-hot-toast';
 
-const queryClient = new QueryClient();
-
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-  const [client] = useState(queryClient);
   return (
     <SessionProvider>
       <ThemeProvider defaultTheme='dark'>
